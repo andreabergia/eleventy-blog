9e2f9b5e9e74ce8766b18ca3d6daa3650f9b13a6
Andrea Bergia <andreabergia@gmail.com>
2026-01-11 17:34:58 +0100

    Update build.test.js

--- test/build.test.js
+++ test/build.test.js
@@ -6,16 +6,12 @@ const path = require('node:path');
 const DIST_DIR = path.join(__dirname, '..', 'dist');
 const SRC_BLOG_DIR = path.join(__dirname, '..', 'src', 'blog');
 
-test('markdown file gets converted to HTML file', () => {
-  const htmlPath = path.join(DIST_DIR, 'blog', '2025', '02', 'emjay-a-simple-jit-that-does-math', 'index.html');
-  assert.ok(fs.existsSync(htmlPath));
-});
-
 test('markdown files are converted to HTML files', () => {
   const htmlPaths = [
     path.join(DIST_DIR, 'blog', '2025', '02', 'emjay-a-simple-jit-that-does-math', 'index.html'),
     path.join(DIST_DIR, 'blog', '2022', '04', 'reboot', 'index.html'),
-    path.join(DIST_DIR, 'blog', '2023', '07', 'i-have-written-a-jvm-in-rust', 'index.html')
+    path.join(DIST_DIR, 'blog', '2023', '07', 'i-have-written-a-jvm-in-rust', 'index.html'),
+    path.join(DIST_DIR, 'about', 'index.html'),
   ];
 
   htmlPaths.forEach(htmlPath => {
@@ -26,10 +22,10 @@ test('markdown files are converted to HTML files', () => {
 test('generated page has post title', () => {
   const htmlPath = path.join(DIST_DIR, 'blog', '2025', '02', 'emjay-a-simple-jit-that-does-math', 'index.html');
   const html = fs.readFileSync(htmlPath, 'utf-8');
-  assert.ok(html.includes('Emjay - a simple JIT that does math'));
+  assert.ok(html.includes('<title>Emjay - a simple JIT that does math</title>'));
 });
 
-test('generated page exists', () => {
+test('generated homepage page exists', () => {
   const indexPath = path.join(DIST_DIR, 'index.html');
   assert.ok(fs.existsSync(indexPath));
 });
